aca3964fa9e5b30d57a5b7bdf2232a255907123c
Rishab kanwar <rk8283824166@gmail.com>
2020-03-21 01:25:41 +0530

    added user view engine and views folder

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const express= require('express');
 const app = express();
 const port = 10000;
 
-app.use('/',require('./routes'))
+app.use('/',require('./routes'));
 
+app.set('view engine','ejs');
+app.set('views','./views');
 
 app.listen(port, function(err){
     if (err) {
