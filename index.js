00c1f2ca8b3aceaff568e05da30d6fd75fca9c6c
Rishab kanwar <rk8283824166@gmail.com>
2020-03-21 14:15:08 +0530

    added database and ejs

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 
 const express= require('express');
 const app = express();
-const port = 10000;
+const port = 12000;
 
 app.use('/',require('./routes'));
 
 app.set('view engine','ejs');
 app.set('views','./views');
+const db = require('./config/mongoose');
+app.use(express.urlencoded());
+app.use(express.static('./front_end'));
+
 
 app.listen(port, function(err){
     if (err) {
         console.log("Error in running the server", err);
     }
     console.log('Yup!My Server is running on Port', port);
-})
+});
---
+++ models/todo.js
@@ -0,0 +1,17 @@
+const mongoose=require('mongoose');
+const todo_Schema= new mongoose.Schema({
+    discription:{
+        type: String,
+        required: true
+    },
+    category:{
+        type: String,
+        required: true
+    },
+    dueDate :{
+        type: Date,
+        required: true
+    }
+});
+    const Todo=mongoose.model('Todo',todo_Schema);
+    module.exports=Todo;
