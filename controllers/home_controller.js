2539197802b5f2d69647202b43ecdf9d86e30ae8
Rishab kanwar <rk8283824166@gmail.com>
2020-03-23 14:05:42 +0530

    added view of Todo list on ejs

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,5 +1,15 @@
-module.exports.home=function(req,res){
-    return res.render('home',{
-        title : 'home'
-    })
+const Todo=require('../models/todo');
+module.exports.home=function(req, res){
+
+    Todo.find({}, function(err, todo_list){
+        if(err){
+            console.log("error in fetching contacts from db");
+            return;
+        }
+        return res.render('home',{
+            title: "TODO List",
+            todo_list: todo_list
+        });
+
+    });
 }
