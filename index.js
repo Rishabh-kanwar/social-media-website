ae534b6bf9b49c6e46a2667be6a0bf78eaf2d128
Rishab kanwar <rk8283824166@gmail.com>
2020-03-28 01:05:11 +0530

    final css and ejs design

--- index.js
+++ index.js
@@ -3,16 +3,22 @@ const express= require('express');
 
 const app = express();
 //defining port
-const port = 12000;
+const port = 13000;
 //acquiring db 
 const db = require('./config/mongoose');
+//to pass the form data
 app.use(express.urlencoded());
-
+//seeting up view engine
 app.set('view engine','ejs');
+//setting up "views" folder for ejs files
 app.set('views','./views');
+//to access the static files such as css,js,images
 app.use(express.static('./front_end'));
+//
+app.locals.mom
+//setting up "routes" folder
 app.use('/',require('./routes'));
-
+ 
 app.listen(port, function(err){
     if (err) {
         console.log("Error in running the server", err);
