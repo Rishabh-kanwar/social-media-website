d47af5c28c403c9c2d46b188749f91c5b439ef44
Rishab kanwar <rk8283824166@gmail.com>
2020-04-01 00:01:33 +0530

    new project socio

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,15 +1,7 @@
-const Todo=require('../models/todo');
 module.exports.home=function(req, res){
 
-    Todo.find({}, function(err, todo_list){
-        if(err){
-            console.log("error in fetching contacts from db");
-            return;
-        }
         return res.render('home',{
             title: "TODO List",
-            todo_list: todo_list
         });
 
-    });
 }
