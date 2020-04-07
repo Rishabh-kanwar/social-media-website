03bcba44a90cab4f357ce406c179c151c112beb2
Rishab kanwar <rk8283824166@gmail.com>
2020-04-07 21:24:47 +0530

    setting up scss and done some styling

--- index.js
+++ index.js
@@ -1,19 +1,33 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const app = express();
-const port = 13000;
+const port = 8000;
 const expressLayouts = require('express-ejs-layouts');
 const db = require('./config/mongoose');
 // used for session cookie
 const session = require('express-session');
 const passport = require('passport');
 const passportLocal = require('./config/passport-local-strategy');
+const MongoStore = require('connect-mongo')(session);
+
+
+//setting up scss in our project
+const sassMiddleware=require('node-sass-middleware');
+app.use(sassMiddleware({
+    src: './assets/scss',
+    dest: './assets/css',
+    debug: true,
+    outputStyle: "expanded",
+    prefix: '/css'
+}));
+
+
 
 app.use(express.urlencoded());
 
 app.use(cookieParser());
 
-app.use(express.static('./front_end'));
+app.use(express.static('./assets'));
 
 app.use(expressLayouts);
 // extract style and scripts from sub pages into the layout
@@ -27,7 +41,7 @@ app.set('layout extractScripts', true);
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
-
+// mongo store is used to store the session cookie in the db
 app.use(session({
     name: 'codeial',
     // TODO change the secret before deployment in production mode
@@ -36,11 +50,23 @@ app.use(session({
     resave: false,
     cookie: {
         maxAge: (1000 * 60 * 100)
-    }
+    },
+    store: new MongoStore(
+        {
+            mongooseConnection: db,
+            autoRemove: 'disabled'
+        
+        },
+        function(err){
+            console.log(err ||  'connect-mongodb setup ok');
+        }
+    )
 }));
 
 app.use(passport.initialize());
 app.use(passport.session());
+
+app.use(passport.setAuthenticatedUser);
 
 // use express router
 app.use('/', require('./routes'));
