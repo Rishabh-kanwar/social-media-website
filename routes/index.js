2bffc69dd3a814a85fba9d8721daebd7383d810a
Rishab kanwar <rk8283824166@gmail.com>
2020-04-08 11:44:37 +0530

    setting up post feature

--- routes/index.js
+++ routes/index.js
@@ -12,5 +12,5 @@ router.use('/users', require('./users'));
 // for any further routes, access from here
 // router.use('/routerName', require('./routerfile));
 
-
+router.use('/posts', require('./posts'));
 module.exports = router;
