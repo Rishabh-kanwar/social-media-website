4bfd97559da7a716cf22076e0188b40e26ac6eb7
Rishab kanwar <rk8283824166@gmail.com>
2020-04-11 20:23:56 +0530

    updation of username and email can be done

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,6 +1,18 @@
 const User = require('../models/user');
 
 
+
+module.exports.update = function(req, res){
+    if(req.user.id == req.params.id){
+        User.findByIdAndUpdate(req.params.id, req.body, function(err, user){
+            return res.redirect('back');
+        });
+    }else{
+        return res.status(401).send('Unauthorized');
+    }
+}
+
+
 module.exports.profile = function(req, res){
     User.findById(req.params.id, function(err,user){
         return res.render('user_profile', {
