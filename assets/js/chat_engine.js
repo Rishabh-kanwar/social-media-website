6c4da69d47eefc49ac5b7c9aec5633440ba61f92
Rishab kanwar <rk8283824166@gmail.com>
2020-06-24 04:59:43 +0530

    some changes

--- assets/js/chat_engine.js
+++ assets/js/chat_engine.js
@@ -3,7 +3,7 @@ class ChatEngine{
         this.chatBox = $(`#${chatBoxId}`);
         this.userEmail = userEmail;
 
-        this.socket = io.connect('http://127.0.0.1:5000');
+        this.socket = io.connect('http://socioo.in:5000');
 
         if (this.userEmail){
             this.connectionHandler();
