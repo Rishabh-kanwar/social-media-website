5e7f1b39dab076115d8f3ac40cb12a0b173a3858
Rishab kanwar <rk8283824166@gmail.com>
2020-06-25 03:18:02 +0530

    save changes

--- controllers/friends_controller.js
+++ controllers/friends_controller.js
@@ -94,23 +94,23 @@ module.exports.destroy = async function(req, res){
     try{
         await User.findByIdAndUpdate(req.user._id,{$pull : { friendships: req.params.id}});
         await User.findByIdAndUpdate(req.params.id,{$pull : { friendships: req.user._id}});
-       let friend= await Friendship.findOne({
-            from_user :req.user._id,
-            to_user : req.params.id
-        });
+    //    let friend= await Friendship.findOne({
+    //         from_user :req.user._id,
+    //         to_user : req.params.id
+    //     });
       
-        if(friend==null)
-        {
-        friend= await Friendship.findOne({
-            from_user :req.params.id,
-            to_user: req.user._id
-        });
-        }
+        // if(friend==null)
+        // {
+        // friend= await Friendship.findOne({
+        //     from_user :req.params.id,
+        //     to_user: req.user._id
+        // });
+        // }
     // console.log(friend,'delete me bro');
     // await Friendship.findByIdAndDelete(friend._id);
-    //     let user = await User.findById(req.params.id);
-    //     req.flash('error','Unfriended ',user.name);
-    //     return res.redirect('back');
+        let user = await User.findById(req.params.id);
+         req.flash('error','Unfriended ',user.name);
+        return res.redirect('back');
     }catch(err)
     {
      console.log("error in send deleting the friend",err);
